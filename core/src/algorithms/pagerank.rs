a633ca45b51cebcdc21c34e93d6c91cb8eed23e2
Cesar Augusto <cesardeazevedo@outlook.com>
2025-12-21 21:07:04 -0300

    Add simple unit tests for global pagerank
    This also make the algorithm more generic,
    so we can test with simple node u64.

--- core/src/algorithms/pagerank.rs
+++ core/src/algorithms/pagerank.rs
@@ -1,5 +1,5 @@
 use differential_dataflow::{
-    AsCollection, VecCollection,
+    AsCollection, ExchangeData, Hashable, VecCollection,
     lattice::Lattice,
     operators::{iterate::Variable, *},
 };
@@ -8,17 +8,19 @@ use timely::{
     order::Product,
 };
 
-use crate::types::{Diff, Edge, Node};
+use crate::types::Diff;
 
 type Iter = u32;
 
 /// For reference: https://github.com/TimelyDataflow/differential-dataflow/blob/master/differential-dataflow/examples/pagerank.rs
-pub fn pagerank<G>(
+pub fn pagerank<G, D>(
     iters: Iter,
-    edges: &VecCollection<G, Edge, Diff>,
-) -> VecCollection<G, Node, Diff>
+    edges: &VecCollection<G, (D, D), Diff>,
+) -> VecCollection<G, D, Diff>
 where
     G: Scope<Timestamp: Lattice>,
+    D: ExchangeData + Hashable,
+    (D, isize): Hashable,
 {
     // initialize many surfers at each node.
     let nodes = edges
@@ -66,4 +68,87 @@ where
         ranks.set(&pushed);
         pushed.leave()
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use differential_dataflow::input::InputSession;
+    use differential_dataflow::operators::CountTotal;
+    use std::sync::mpsc::channel;
+    use timely::dataflow::operators::capture::{Capture, Extract};
+    use timely::execute_directly;
+
+    #[test]
+    fn assert_pagerank() {
+        let (tx, rx) = channel();
+
+        execute_directly(move |worker| {
+            let mut edges_input: InputSession<u64, (u32, u32), isize> = InputSession::new();
+
+            worker.dataflow::<u64, _, _>(|scope| {
+                let edges = edges_input.to_collection(scope);
+                let ranks = pagerank(20, &edges).count_total();
+
+                ranks.inner.capture_into(tx);
+            });
+
+            edges_input.advance_to(1);
+            let graph_edges = vec![(1, 2), (2, 1), (3, 1), (4, 1), (5, 1)];
+
+            for edge in graph_edges {
+                edges_input.insert(edge);
+            }
+
+            edges_input.flush();
+
+            edges_input.advance_to(2);
+            // node1 unfollows node2
+            edges_input.remove((1, 2));
+            edges_input.flush();
+
+            edges_input.advance_to(3);
+            // node1 follows node2 again
+            edges_input.insert((1, 2));
+            edges_input.flush();
+        });
+
+        let mut ranks: Vec<(u32, u64, isize, isize)> = rx
+            .extract()
+            .into_iter()
+            .flat_map(|(_cap_ts, batch)| {
+                batch
+                    .into_iter()
+                    .map(|((node, rank), ts, diff)| (node, ts, rank, diff))
+            })
+            .collect();
+        ranks.sort_by_key(|(_, ts, _, _)| *ts);
+
+        let get_ranks_at_time = |time: u64| {
+            ranks
+                .iter()
+                .filter(|(_, ts, _, diff)| *ts == time && *diff > 0)
+                .map(|(n, _, r, _)| (*n, *r))
+                .collect::<Vec<_>>()
+        };
+
+        let ranks_t1 = get_ranks_at_time(1);
+        let ranks_t2 = get_ranks_at_time(2);
+        let ranks_t3 = get_ranks_at_time(3);
+
+        assert_eq!(
+            ranks_t1,
+            vec![
+                (1, 13968397),
+                (2, 13031592),
+                (3, 1000000),
+                (4, 1000000),
+                (5, 1000000),
+            ]
+        );
+        // Remove edge (1,2) causes rank of 2 to drop.
+        // We also want to assure that node 3,4,5 ranks remain unchanged.
+        assert_eq!(ranks_t2, vec![(1, 4333332), (2, 1000000)]);
+        assert_eq!(ranks_t3, vec![(1, 13968397), (2, 13031592)]);
+    }
 }
