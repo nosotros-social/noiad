e979cecfd093db35b73fd74275d854c74c880d10
Cesar Augusto <cesardeazevedo@outlook.com>
2026-01-11 21:56:25 -0300

    Keep materialize header licenses on builder_async.rs and probe.rs

--- core/src/operators/builder_async.rs
+++ core/src/operators/builder_async.rs
@@ -1,5 +1,21 @@
-// An asynchronous operator builder for timely dataflow.
-// mostly from https://github.com/MaterializeInc/materialize/blob/main/src/timely-util/src/builder_async.rs
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE file at the
+// root of this repository, or online at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// based on: https://github.com/MaterializeInc/materialize/blob/main/src/timely-util/src/builder_async.rs
+//
+//! An asynchronous operator builder for timely dataflow.
 use std::cell::{Cell, RefCell};
 use std::collections::VecDeque;
 use std::future::Future;
--- core/src/operators/probe.rs
+++ core/src/operators/probe.rs
@@ -1,4 +1,19 @@
-// https://github.com/MaterializeInc/materialize/blob/main/src/timely-util/src/probe.rs
+// Copyright Materialize, Inc. and contributors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License in the LICENSE file at the
+// root of this repository, or online at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// from: https://github.com/MaterializeInc/materialize/blob/main/src/timely-util/src/probe.rs
 use std::cell::RefCell;
 use std::convert::Infallible;
 use std::rc::Rc;
@@ -99,7 +114,7 @@ impl<T: Timestamp> Handle<T> {
     /// # Examples
     ///
     /// ```
-    /// use mz_timely_util::probe::Handle;
+    /// use crate::core::operators::probe::Handle;
     ///
     /// let handle = Handle::<usize>::default();
     /// let frontier = handle.with_frontier(|frontier| frontier.to_vec());
